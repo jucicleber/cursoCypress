55478d19f72b54db55301133ecfdf51bd3ab394b
Jucicleber da Silva Gomes <71895344+jucicleber@users.noreply.github.com>
2024-03-21 00:43:09 -0400

    gg

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />///
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    it('verifica o título da aplicação', function() {
+        cy.visit('./src/index.html')
+
+        cy.title('').should('be.equals', 'Central de Atendimento ao Cliente TAT')
+    })
+  })
