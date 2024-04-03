f5c3e3359779bc1557d862d47e12c5dabe5592d7
Jucicleber da Silva Gomes <71895344+jucicleber@users.noreply.github.com>
2024-04-02 23:47:21 -0400

    atalização

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -21,7 +21,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible')
     })
 //Email inválido
-    it.only('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
         cy.get('#firstName').type('Creber')
         cy.get('#lastName').type('Silva')
         //o email neste cenário tem uma virgula ficando invalido
@@ -32,4 +32,24 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         //Mensagem de erro...
         cy.get('.error').should('be.visible')
     })
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function(){
+       //inserindo um texto no campo telefone que só aceita, atravez de uma função
+       //assim vazio por nãol ter número, depois o should valida que o campo esta vazio
+       cy.get('#phone')
+        .type('aaaaaaaaa')
+        .should('have.value', '')
+    })
+    it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido', function(){
+        cy.get('#firstName').type('Creber')
+        cy.get('#lastName').type('Silva')
+        cy.get('#email').type('creber.silva@yahoo.com')
+        //marcando o checkbox para o telefone ficar vazio depois não preenche
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('teste')
+        //quando clica no botão mostra o erro
+        cy.get('button[type="submit"]').click()
+        //Mensagem de erro...
+        cy.get('.error').should('be.visible')
+    })
+
 })
