df13dfc82fc24aadd6dcaa995559659d13c2487b
Jucicleber da Silva Gomes <71895344+jucicleber@users.noreply.github.com>
2024-09-23 17:29:25 -0400

    testejucicleber

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -102,7 +102,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             //averiguando pelo value
             .should('have.value','blog')
     })
-    it.only('Seleciona um arquivo da pasta fixtures', function (){
+    it('Seleciona um arquivo da pasta fixtures', function (){
         cy.get('input[type="file"]#file-upload').should('not.have.value').selectFile('./cypress/fixtures/example.json')
         .should(function($input){
             expect($input[0].files[0].name).to.equal('example.json')
