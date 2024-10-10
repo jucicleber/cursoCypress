5f84c1f450035f7935357cf30e8f5281d3765576
Jucicleber da Silva Gomes <71895344+jucicleber@users.noreply.github.com>
2024-10-09 20:31:48 -0400

    teste

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,6 @@ Cypress.Commands.add('fillMandatoryFieldAndSubmit', function(){
     cy.get('#firstName').type('Creber')
     cy.get('#lastName').type('Silva')
     cy.get('#email').type('creber.silva@yahoo.com')
-    cy.get('#phone').type('65999990000')
     cy.get('#open-text-area').type('Teste')
-    cy.contains('button','Enviar').click()
+    cy.get('button[type="submit"]').click()
 })
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,5 @@
+import './commands';
+
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
